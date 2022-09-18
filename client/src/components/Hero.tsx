36c93c8012eb264e39eab2cba3f46beaaa036931
parrth <choudharyparth21@gmail.com>
2022-09-18 17:51:54 +0530

    browse fix

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -17,9 +17,9 @@ function Hero() {
 
 
   return (
-    <div className = 'text-white z-10 overflow-hidden w-full h-2/4 bg-black bg-opacity-70 relative flex-col rounded-b-3xl shadow-md shadow-gray-500'>
-        <div className = { `absolute w-full h-full top-0 left-0 bg-[url('../src/images/hero.jpg')] opacity-90 bg-cover mix-blend-overlay`}></div>
-        <div className = { `w-full top-1/4 absolute left-1/4`}>
+    <div className = 'text-white z-10 w-full h-2/4 bg-black bg-opacity-70 relative flex-col rounded-b-3xl shadow-md shadow-gray-500'>
+        <div className = { `absolute w-full h-full top-0 left-0 bg-[url('../src/images/hero.jpg')] rounded-b-3xl opacity-90 bg-cover mix-blend-overlay`}></div>
+        <div className = { `w-full top-1/4 absolute pl-80`}>
             <h1 className='text-6xl font-bold'>BeverageDB</h1>
             <p className ='text-sm text-gray-200 mt-3'>An online resource for biological information on beverages</p>
             <div className='flex items-center mt-5'>
@@ -28,7 +28,7 @@ function Hero() {
                 <IoMdArrowDropdown className = {isBrowseOpen? 'openDropdownIcon': 'closeDropdownIcon'}/>
               </button>
               <form className = 'w-full flex items-center'>
-                <input placeholder={placeholderText} className = 'font-nunito w-2/6 py-1 focus:outline-none text-gray-600 indent-3' type = 'text' />
+                <input placeholder={placeholderText} className = 'font-nunito w-3/6 py-1 focus:outline-none text-gray-600 indent-3' type = 'text' />
                 <button className = 'bg-red-400 w-32 py-1 px-4 rounded-r-full flex gap-1 items-center'>
                   <AiOutlineSearch className = 'text-xl' />Search
                 </button>
