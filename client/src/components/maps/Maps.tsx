881ddc278910660d34ef34dc3ba80ccf5872706a
Arjun Tanwar <arjunthakur900@gmail.com>
2022-11-20 11:34:44 +0530

    changes

--- client/src/components/maps/Maps.tsx
+++ client/src/components/maps/Maps.tsx
@@ -51,7 +51,7 @@ function Maps() {
         x++;
         if(!datalist.includes(feature?.properties.NAME_1)) datalist.push(feature?.properties.NAME_1)
         return {
-            fillColor: colorArray[colorArray.length - 1 - x],
+            fillColor: colorArray[x % (colorArray.length)],
             fillOpacity: selected === feature?.properties.NAME_1? 0.8: 0.3,
             color: "gray",
             weight: selected === feature?.properties.NAME_1? 2: 0.3
