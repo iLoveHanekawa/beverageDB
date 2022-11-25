af6904451113f7129432761fc1075038f43c5d2c
Arjun Tanwar <arjunthakur900@gmail.com>
2022-11-25 11:48:55 +0530

    push

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000/"
-      }
-    }
   },
   plugins: [react()]
 })
